04fcbb05dd7a531eaed8dd383722e3a7c945228d
Tugsem <saranlitugsem@gmail.com>
2022-06-09 12:41:26 +0300

    Add local react state using hooks

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,34 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'tachyons';
 import { useDispatch } from 'react-redux';
 import { addBook } from '../redux/books/book';
 
 const Form = () => {
   const dispatch = useDispatch();
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
 
   return (
     <div className="formContainer">
       <legend id="form-title">ADD NEW BOOK</legend>
       <form className="d-flex">
         <label htmlFor="title">
-          <input type="text" placeholder="Book Title" id="title" />
+          <input type="text" placeholder="Book Title" id="title" value={title} onChange={(e) => setTitle(e.target.value)} />
         </label>
         <label htmlFor="author">
-          <input type="text" placeholder="Author" id="author" />
+          <input type="text" placeholder="Author" id="author" value={author} onChange={(e) => setAuthor(e.target.value)} />
         </label>
         <button
           type="button"
           className="formBtn pointer"
           onClick={() => {
-            const titleInp = document.getElementById('title');
-            const authorInp = document.getElementById('author');
-            dispatch(
-              addBook({
-                title: titleInp.value,
-                author: authorInp.value,
-                id: Date.now(),
-              }),
-            );
+            dispatch(addBook({ title, author, id: Date.now() }));
+            setTitle('');
+            setAuthor('');
           }}
         >
           ADD BOOK
